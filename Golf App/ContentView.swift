59dfde21ea4d53287b048ff5bdd05eaecb492cda
LandonB5 <landon.b5@icloud.com>
2024-06-17 18:19:09 -0400

    Initial Commit

---
+++ Golf App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Golf App
+//
+//  Created by Landon Burchill on 6/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Golf App/Golf_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Golf_AppApp.swift
+//  Golf App
+//
+//  Created by Landon Burchill on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Golf_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
